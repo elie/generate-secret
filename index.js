de5fff90831e2a54f49f49ac1c3c1ac51125d4f0
Elie Schoppik <eschoppik@gmail.com>
2016-04-12 11:49:43 -0700

    making it a global module
    adding default values
    
    more readme notes
    
    removing optimist
    
    cleanup
    
    new version
    
    fixing issue with defaults
    
    new version
    
    removing log
    
    adding .env to gitignore

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 #!/usr/bin/env node
 
+'use strict'
+
 const fs = require("fs")
 const prompt = require('prompt');
 
 const addToken = (token) => {
-  prompt.start();
-  var schema = {
-    properties: {
-      filename: {
-        description: 'Enter the name of the file you would like to write the secret to \n Default is .env',
-        default: '.env'
-      },
-    }
-  };
-  prompt.get(schema, function (err, result) {
-    fs.appendFile(result.filename, `SECRET=${token}\n`, 'utf8');
-  })
+  if(process.argv.indexOf('--yes'.toLowerCase()) !== -1 || process.argv.indexOf('--yes'.toLowerCase()) !== -1){
+    fs.appendFile('.env', `SECRET=${token}\n`, 'utf8');
+  }
+  else {
+    let schema = {
+      properties: {
+        filename: {
+          description: 'Enter the name of the file you would like to write the secret to \n Default is .env',
+          default: '.env'
+        },
+        key: {
+          description: 'Enter the name of the key you would like\n Default is SECRET',
+          default: 'SECRET'
+        }
+      }
+    };
+    prompt.get(schema, function (err, result) {
+      fs.appendFile(result.filename, `${result.key}=${token}\n`, 'utf8');
+    })
+  }
 }
 
 require('crypto').randomBytes(48, function(err, buffer) {
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 ### Node Secret
 
-A simple tool to add a key of 'SECRET' to a file using the crypto library's randomBytes function.  
+A simple tool to add a key (default is 'SECRET') to a file (default is `.env`) using the crypto library's randomBytes function.
+
+Install the module globally with `npm install -g generate-secret`
+
+Run using `generate-secret` and specify the file and key or override using `--yes` to use the default values
